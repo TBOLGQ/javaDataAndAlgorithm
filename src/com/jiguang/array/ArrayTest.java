27c9e46b5b9ce490b967f77d5ec959b5d4f19811
liguangqi <18811635753@163.com>
2021-04-06 16:58:17 +0800

    java数据结构测试

---
+++ src/com/jiguang/array/ArrayTest.java
@@ -0,0 +1,17 @@
+package com.jiguang.array;
+
+import java.util.Arrays;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{9,8,7};
+        System.out.println(Arrays.toString(arr1));
+        int[] newArr1 = new int[arr1.length+1];
+        for (int i=0;i< arr1.length;i++){
+            newArr1[i] = arr1[i];
+        }
+        newArr1[arr1.length] = 6;
+        arr1 = newArr1;
+        System.out.println(Arrays.toString(arr1));
+    }
+}
