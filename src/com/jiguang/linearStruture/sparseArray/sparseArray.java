26f99fd84560ae8313591a146ba094ca402e9d9c
liguangqi <18811635753@163.com>
2021-04-29 10:17:58 +0800

    fix:删除文件

---
+++ src/com/jiguang/linearStruture/sparseArray/sparseArray.java
@@ -0,0 +1,58 @@
+package com.jiguang.linearStruture.sparseArray;
+
+public class sparseArray {
+    public static void main(String[] args) {
+        int[][] chessArray1 = new int[11][11];
+        chessArray1[1][2] = 1;
+        chessArray1[2][3] = 2;
+        System.out.println("原始的二位数组:");
+        for (int[] row:chessArray1){
+            for (int data:row) {
+                System.out.printf("%d\t",data);
+            }
+            System.out.println();
+        }
+        int sum = 0;
+        for (int i=0;i< chessArray1.length;i++){
+            for (int j=0;j<chessArray1[i].length;j++){
+                if (chessArray1[i][j] != 0){
+                    sum++;
+                }
+            }
+        }
+        System.out.println("非0值的个数:"+sum);
+        int[][] sparseArray = new int[sum+1][3];
+        sparseArray[0][0] = chessArray1.length;
+        sparseArray[0][1] = chessArray1[0].length;
+        sparseArray[0][2] = sum;
+        int count = 0;
+        for (int i=0;i< chessArray1.length;i++){
+            for (int j=0;j<chessArray1[i].length;j++){
+                if (chessArray1[i][j] != 0){
+                    count++;
+                    sparseArray[count][0] = i;
+                    sparseArray[count][1] = j;
+                    sparseArray[count][2] = chessArray1[i][j];
+                }
+            }
+        }
+        System.out.println("转换后的稀疏数组:");
+        for (int[] row:sparseArray){
+            for (int data:row){
+                System.out.printf("%d\t",data);
+            }
+            System.out.println();
+        }
+        int[][] chessArray2 = new int[sparseArray[0][0]][sparseArray[0][1]];
+        for (int i=1;i<sparseArray.length;i++){
+            chessArray2[sparseArray[i][0]][sparseArray[i][1]] = sparseArray[i][2];
+        }
+        System.out.println("恢复后的数组:");
+        for (int[] row:chessArray2){
+            for (int data:row) {
+                System.out.printf("%d\t",data);
+            }
+            System.out.println();
+        }
+    }
+}
